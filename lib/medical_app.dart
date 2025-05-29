4dca84c2304796153bb88f3666e086c6218a35c4
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-05-29 21:59:51 +0300

    Configures routing for the app
    Configures the app's routing system using `onGenerateRoute` and sets the initial route to the onboarding view.
    This enables navigation between different screens of the application.

--- lib/medical_app.dart
+++ lib/medical_app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:medical_project/core/routing/router_manger.dart';
+import 'package:medical_project/core/routing/router_name.dart';
 
 class MedicalApp extends StatelessWidget {
   const MedicalApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: ongenerateroute,
+      initialRoute: RouterName.onBoardingView,
+    );
   }
 }
