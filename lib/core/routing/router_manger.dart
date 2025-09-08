dda37ae75375f0b4da2c923a986b6db9b0dec898
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-09-08 15:31:45 +0300

    feat (auth) : implement blocprovider on the signinview

--- lib/core/routing/router_manger.dart
+++ lib/core/routing/router_manger.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medical_project/core/di/depedency_injection.dart';
 import 'package:medical_project/core/routing/router_name.dart';
+import 'package:medical_project/features/auth/presentation/logic/manager/login_cubit/login_cubit.dart';
 import 'package:medical_project/features/auth/presentation/views/forgot_password_view.dart';
 import 'package:medical_project/features/auth/presentation/views/sign_in_view.dart';
 import 'package:medical_project/features/auth/presentation/views/sign_up_view.dart';
@@ -11,7 +14,12 @@ MaterialPageRoute ongenerateroute(RouteSettings settings) {
       return MaterialPageRoute(builder: (_) => const OnBoardingView());
 
     case RouterName.signin:
-      return MaterialPageRoute(builder: (_) => const SignInView());
+      return MaterialPageRoute(
+        builder: (_) => BlocProvider(
+          create: (context) => getIt<LoginCubit>(),
+          child: const SignInView(),
+        ),
+      );
 
     case RouterName.signup:
       return MaterialPageRoute(builder: (_) => const SignUpView());
@@ -20,7 +28,9 @@ MaterialPageRoute ongenerateroute(RouteSettings settings) {
       return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
     default:
       return MaterialPageRoute(
-        builder: (_) => Center(child: Text('No Route Found')),
+        builder: (_) => Scaffold(
+          body: Center(child: Text('No route defined for ${settings.name}')),
+        ),
       );
   }
 }
